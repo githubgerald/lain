eb44459824ebdfd46290b6ad7650287460b6a62a
Cameron <130686773+githubgerald@users.noreply.github.com>
2025-11-11 07:56:42 +0000

    Chat / Share buttons
    > Added functionality so only one can be on at a time
    - Good positioning but its fake, only put there by random margins, need to find a better solution.
    > JavaScript seems simple but can probably be optimised.

--- scripts.js
+++ scripts.js
@@ -8,3 +8,24 @@ area.addEventListener('input', function () {
     let content = this.value;
     char.textContent = content.length;
 });
+
+// Chat / Share mode selector
+
+let chat = document.getElementById('chatBtn');
+let share = document.getElementById('shareBtn');
+
+chat.addEventListener("change", function() {
+    if (this.checked) {
+        share.checked = false
+        share.disabled = false
+        this.disabled = true
+    }
+})
+
+share.addEventListener("change", function() {
+    if (this.checked) {
+        chat.checked = false
+        chat.disabled = false
+        this.disabled = true
+    }
+})
