d2687a5173fdfb545be72824b471da6382e87d50
28dhu2nujncjdawi <160336851+28dhu2nujncjdawi@users.noreply.github.com>
2025-11-13 21:36:22 +0000

    fixed temporary button click js

--- static/scripts.js
+++ static/scripts.js
@@ -7,7 +7,7 @@ function myFunction() {
 // GET request the chatlog
 
 async function GETchatlog(url) {
-    const response = await fetch("http://localhost:5000/api/chats/1234");
+    const response = await fetch("http://localhost:5000/api/v0/chats/1234");
     var chatlog = await response.json();
     console.log(chatlog)
 }
