1bf5cde031bf34f35e32452bd2edc889b356b73e
Crambor <me@crambor.net>
2025-11-13 20:25:31 +0000

    refactor: ruff fmt server code

--- chat_server.py
+++ chat_server.py
@@ -3,24 +3,26 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 server = Flask(__name__)
-CORS(server) # https://stackoverflow.com/a/46637194
+CORS(server)  # https://stackoverflow.com/a/46637194
 
-@server.route('/api/chats/<int:chat_id>', methods=['GET'])
+
+@server.route("/api/chats/<int:chat_id>", methods=["GET"])
 def get_chat(chat_id):
     response = []
-    with open("./chats/"+str(chat_id)+".txt") as file:
+    with open("./chats/" + str(chat_id) + ".txt") as file:
         for line in file:
-            response.append(line.rstrip()) # append to array and strip of newline
-    return jsonify({"messages":response})
+            response.append(line.rstrip())  # append to array and strip of newline
+    return jsonify({"messages": response})
 
-@server.route('/api/chats/<int:chat_id>', methods=['POST'])
+
+@server.route("/api/chats/<int:chat_id>", methods=["POST"])
 def receive_msg(chat_id):
-    new_msg = request.json['msg']
-    chOpen = open("./chats/"+str(chat_id)+".txt", "a")
-    chOpen.write(new_msg+"\n")
+    new_msg = request.json["msg"]
+    chOpen = open("./chats/" + str(chat_id) + ".txt", "a")
+    chOpen.write(new_msg + "\n")
     chOpen.close()
-    return jsonify({'messaged': new_msg}), 201
+    return jsonify({"messaged": new_msg}), 201
+
 
 if __name__ == "__main__":
     server.run(debug=False)
-    
