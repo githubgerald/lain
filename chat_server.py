d86f4ad7b1b2a1f0e293edadde1fe8f0d78d62db
28dhu2nujncjdawi <160336851+28dhu2nujncjdawi@users.noreply.github.com>
2025-11-13 21:11:42 +0000

    webcli stuff
    moved web client files into its own folder and made chat_server.py host it

--- chat_server.py
+++ chat_server.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 
 server = Flask(__name__)
 CORS(server)  # https://stackoverflow.com/a/46637194
 
 
-@server.route("/api/chats/<int:chat_id>", methods=["GET"])
+@server.route("/api/v0/chats/<int:chat_id>", methods=["GET"])
 def get_chat(chat_id):
     response = []
     with open("./chats/" + str(chat_id) + ".txt") as file:
@@ -14,8 +14,7 @@ def get_chat(chat_id):
             response.append(line.rstrip())  # append to array and strip of newline
     return jsonify({"messages": response})
 
-
-@server.route("/api/chats/<int:chat_id>", methods=["POST"])
+@server.route("/api/v0/chats/<int:chat_id>", methods=["POST"])
 def receive_msg(chat_id):
     new_msg = request.json["msg"]
     chOpen = open("./chats/" + str(chat_id) + ".txt", "a")
@@ -23,6 +22,9 @@ def receive_msg(chat_id):
     chOpen.close()
     return jsonify({"messaged": new_msg}), 201
 
+@server.route("/")
+def index():
+    return send_from_directory("static", "index.html")
 
 if __name__ == "__main__":
     server.run(debug=False)
